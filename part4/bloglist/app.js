393e74591ae51efa1d1dcecfbb23260d302cfe92
roopeaal <roope.aaltonen@metropolia.fi>
2026-08-12 10:23:53 +0300

    Complete exercise 4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -29,6 +29,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,19 +3,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-
-  if (
-    authorization &&
-    authorization.startsWith('Bearer ')
-  ) {
-    return authorization.replace('Bearer ', '')
-  }
-
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
@@ -31,7 +18,7 @@ blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   const decodedToken = jwt.verify(
-    getTokenFrom(request),
+    request.token,
     process.env.SECRET
   )
 
