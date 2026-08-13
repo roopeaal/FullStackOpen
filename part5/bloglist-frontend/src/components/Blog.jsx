ea0e205df8999fd655652602a04381fb33bc241e
roopeaal <roope.aaltonen@metropolia.fi>
2026-08-13 12:33:32 +0300

    Complete exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+
+          <div>
+            likes {blog.likes}{' '}
+            <button type="button">
+              like
+            </button>
+          </div>
+
+          <div>
+            {blog.user?.name || ''}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
