ffbd8f300f9bb3602a8733d7a1bfc429689193fd
roopeaal <roope.aaltonen@metropolia.fi>
2026-08-14 10:52:20 +0300

    Complete exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -193,13 +193,15 @@ const App = () => {
         </div>
       )}
 
-      {blogs.map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          updateBlog={updateBlog}
-        />
-      )}
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+            updateBlog={updateBlog}
+          />
+        )}
     </div>
   )
 }
